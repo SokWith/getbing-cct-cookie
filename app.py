0a47cc10828f2a53d15748ff682b28ba685f247b
SokWith <129967025+SokWith@users.noreply.github.com>
2024-02-02 14:13:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,10 +29,8 @@ def get_cookies():
       # cookies = ';'.join([cookie.split(';')[0] for cookie in cookies.split(';')])
       # cookies = ';'.join([re.sub(r';.*', '', cookie) for cookie in cookies.split(';')])
       
-      # 发送请求，获取响应
-      # response = requests.get("https://www.example.com")
       # 从响应头中提取 set-cookies 列表
-      set_cookies = response.headers.getlist('Set-Cookie')
+      set_cookies = response.headers.get('Set-Cookie')
       # 用分号 ; 连接 set-cookies 的键值对，忽略附属属性
       cookie_str = ';'.join([item.split(';')[0] for item in set_cookies])
       # 赋值给变量 cookies
